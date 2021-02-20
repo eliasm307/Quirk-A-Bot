17b6b9c6e05b3bd744d16dada3992932f5fd91ec
Eli <13420359+eliasm307@users.noreply.github.com>
2021-02-20 14:09:08 +0000

    saved c-sheet auto save tested, setup failing test for unrelated instance update

--- packages/data-model/src/classes/CharacterSheet.test.ts
+++ packages/data-model/src/classes/CharacterSheet.test.ts
@@ -2,27 +2,51 @@ import fs from 'fs-extra';
 import path from 'path';
 import CharacterSheet from './CharacterSheet';
 
-test('save new blank character sheet and load the character sheet', () => {
-	const testUserId = Math.floor(Math.random() * 999999);
+const testUserId = Math.floor(Math.random() * 999999);
+const filePath = path.resolve(__dirname, `../data/character-sheets/temporary/${testUserId}.json`);
 
-	const filePath = path.resolve(__dirname, `../data/character-sheets/temporary/${testUserId}.json`);
+let testName: string;
 
-  // creates new sheet and does initial save
+testName = 'save new blank character sheet and load the character sheet';
+
+test(testName, () => {
+	// creates new sheet and does initial save
 	const cs = new CharacterSheet(testUserId, filePath);
- 
 
 	const csLoaded = CharacterSheet.loadFromFile({ filePath });
 
-	console.log({ testUserId, filePath, cs, csLoaded });
+	console.log({ testName, testUserId, filePath, cs, csLoaded });
 
-  // file should exist after save
+	// file should exist after save
 	expect(fs.pathExistsSync(filePath)).toBe(true);
 
-  // user id should be the same
+	// user id should be the same
 	expect(csLoaded.discordUserId).toEqual(testUserId);
 
-  // sheets should be the same
+	// sheets should be the same
 	expect(cs).toEqual(csLoaded);
+});
+
+testName = 'test autosave';
+test(testName, () => {
+	const cs = CharacterSheet.loadFromFile({ filePath });
+
+	const cs2 = CharacterSheet.loadFromFile({ filePath });
+
+  const testHealthValue = 3;
+
+	// test change with auto save
+	cs.health = testHealthValue;
+
+	const csLoaded = CharacterSheet.loadFromFile({ filePath });
+
+	console.log({ testName, csLoadedHealth: csLoaded.health });
+
+	// health should be as specified
+	expect(csLoaded.health).toEqual(testHealthValue);
+
+	// separate instance should update // ? is this good?
+	expect(cs2.health).toEqual(testHealthValue);
 });
 
 // todo test what happens if file doesnt exist
