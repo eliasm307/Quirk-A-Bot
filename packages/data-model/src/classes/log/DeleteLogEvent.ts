057b1f407c2ac40b53bece1838827171f4372534
Eli <13420359+eliasm307@users.noreply.github.com>
2021-02-23 00:01:43 +0000

    with DeleteLogEvent

--- packages/data-model/src/classes/log/DeleteLogEvent.ts
+++ packages/data-model/src/classes/log/DeleteLogEvent.ts
@@ -3,14 +3,14 @@ import BaseLogEvent from './BaseLogEvent';
 
 interface iProps<T> extends iBaseLogEventProps, iOldValue<T> {}
 
-export default class AddLogEvent<T> extends BaseLogEvent<T> implements iLogEvent<T>, iNewValue<T> {
-	newValue: T;
+export default class DeleteLogEvent<T> extends BaseLogEvent<T> implements iLogEvent<T>, iOldValue<T> {
+	oldValue: T;
 
-	constructor({ description, newValue, property }: iProps<T>) {
-		super({ operation: 'ADD', property, description });
-		this.newValue = newValue;
+	constructor({ description, oldValue, property }: iProps<T>) {
+		super({ operation: 'DELETE', property, description });
+		this.oldValue = oldValue;
 	}
 	describe(): string {
-		return `${this.property} added with inital value of ${this.newValue}`;
+		return `${this.property} deleted with last value of ${this.oldValue}`;
 	}
 }
