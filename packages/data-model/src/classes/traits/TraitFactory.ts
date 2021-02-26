8c623c08ab27e1733fece09f3aa135123ba2fcbc
Eli <13420359+eliasm307@users.noreply.github.com>
2021-02-26 22:56:07 +0000

    added getSkillTrait to trait factory

--- packages/data-model/src/classes/traits/TraitFactory.ts
+++ packages/data-model/src/classes/traits/TraitFactory.ts
@@ -1,4 +1,4 @@
-import { AttributeName, AttributeCategory, DisciplineName } from './../../declarations/types';
+import { AttributeName, AttributeCategory, DisciplineName, SkillName } from './../../declarations/types';
 import {
 	iNumberTraitWithCategoryProps,
 	iTraitData,
@@ -7,7 +7,9 @@ import {
 	iDiscipline,
 	iAttributeData,
 	iNumberTraitProps,
-  iDisciplineData,
+	iDisciplineData,
+	iSkill,
+	iSkillData,
 } from './../../declarations/interfaces/trait-interfaces';
 import { iCanHaveSaveAction } from '../../declarations/interfaces/general-interfaces';
 import NumberTraitWithCategory from './NumberTraitWithCategory';
@@ -19,13 +21,14 @@ interface iGetAttributeProps
 		iHasCategorySelector<AttributeName, AttributeCategory> {}
 
 interface iGetDisciplineProps extends iDisciplineData, iCanHaveSaveAction {}
+interface iGetSkillProps extends iSkillData, iCanHaveSaveAction {}
 
 export default abstract class TraitFactory {
 	static getAttributeTrait({ name, value, saveAction, categorySelector }: iGetAttributeProps): iAttribute {
 		const props: iNumberTraitWithCategoryProps<AttributeName, AttributeCategory> = {
 			categorySelector,
-			max: 5,
 			min: 1,
+			max: 5,
 			name,
 			value,
 			saveAction,
@@ -36,8 +39,20 @@ export default abstract class TraitFactory {
 
 	static getDisciplineTrait({ name, value, saveAction }: iGetDisciplineProps): iDiscipline {
 		const props: iNumberTraitProps<DisciplineName> = {
-			max: 5,
 			min: 1,
+			max: 5,
+			name,
+			value,
+			saveAction,
+		};
+
+		return new NumberTrait(props);
+	}
+
+	static getSkillTrait({ name, value, saveAction }: iGetSkillProps): iSkill {
+		const props: iNumberTraitProps<SkillName> = {
+			min: 0,
+			max: 5,
 			name,
 			value,
 			saveAction,
