91b715367e7ff98074e9ac73f50432a868032cd0
Eli <13420359+eliasm307@users.noreply.github.com>
2021-02-26 22:57:24 +0000

    renamed factory methods from get to new

--- packages/data-model/src/classes/traits/TraitFactory.ts
+++ packages/data-model/src/classes/traits/TraitFactory.ts
@@ -24,7 +24,7 @@ interface iGetDisciplineProps extends iDisciplineData, iCanHaveSaveAction {}
 interface iGetSkillProps extends iSkillData, iCanHaveSaveAction {}
 
 export default abstract class TraitFactory {
-	static getAttributeTrait({ name, value, saveAction, categorySelector }: iGetAttributeProps): iAttribute {
+	static newAttributeTrait({ name, value = 0, saveAction, categorySelector }: iGetAttributeProps): iAttribute {
 		const props: iNumberTraitWithCategoryProps<AttributeName, AttributeCategory> = {
 			categorySelector,
 			min: 1,
@@ -37,7 +37,7 @@ export default abstract class TraitFactory {
 		return new NumberTraitWithCategory(props);
 	}
 
-	static getDisciplineTrait({ name, value, saveAction }: iGetDisciplineProps): iDiscipline {
+	static newDisciplineTrait({ name, value = 0, saveAction }: iGetDisciplineProps): iDiscipline {
 		const props: iNumberTraitProps<DisciplineName> = {
 			min: 1,
 			max: 5,
@@ -49,7 +49,7 @@ export default abstract class TraitFactory {
 		return new NumberTrait(props);
 	}
 
-	static getSkillTrait({ name, value, saveAction }: iGetSkillProps): iSkill {
+	static newSkillTrait({ name, value = 0, saveAction }: iGetSkillProps): iSkill {
 		const props: iNumberTraitProps<SkillName> = {
 			min: 0,
 			max: 5,
