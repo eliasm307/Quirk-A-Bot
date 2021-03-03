510096a0f85fae0a543924562bc5d86066acd66b
Eli <13420359+eliasm307@users.noreply.github.com>
2021-03-03 19:17:06 +0000

    added discipline names from quick guide

--- packages/data-model/src/constants.ts
+++ packages/data-model/src/constants.ts
@@ -37,8 +37,29 @@ export const SKILL_NAMES = [
 	'Politics',
 	'Science',
 	'Technology',
-] as const;  
-export const DISCIPLINE_NAMES = ['Animalism', 'Auspex', 'Prescence'] as const; // todo add other explicitly specified names
+] as const;
+export const DISCIPLINE_NAMES = [
+	'Animalism',
+	'Auspex',
+	'Prescence',
+	'Blood Sorcery',
+	'Celerity',
+	'Chimerstry',
+	'Dementation',
+	'Dominate',
+	'Fortitude',
+	'Necromancy',
+	'Obfuscate',
+	'Oblivion',
+	'Obtenebration',
+	'Potence',
+	'Protean',
+	'Quietus',
+	'Serpentis',
+	'Thaumaturgy',
+	'Thin-Blood Alchemy',
+	'Vicissitude',
+] as const; 
 export const ATTRIBUTE_CATEGORIES = ['Physical', 'Social', 'Mental'] as const;
 export const CORE_NUMBER_TRAITS = ['Health', 'Willpower', 'Hunger', 'Humanity', 'Blood Potency', 'Sire'] as const;
 export const CORE_STRING_TRAITS = ['Name', 'Clan', 'Sire'] as const;
