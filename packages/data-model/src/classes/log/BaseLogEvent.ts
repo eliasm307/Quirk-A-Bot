a66796f5b58e8f72a92a8bb57d127b7213ae15bc
Eli <13420359+eliasm307@users.noreply.github.com>
2021-03-13 00:26:16 +0000

    trying to use strategy pattern for logs, this produces issues such as having to make new and old value nullable, so more type checks would be required and there are other issues, its better to have separate classes  with specific responsibilities

--- packages/data-model/src/classes/log/BaseLogEvent.ts
+++ packages/data-model/src/classes/log/BaseLogEvent.ts
@@ -1,35 +1,48 @@
 import { LogOperationUnion } from '../../declarations/types';
 import generateId from '../../utils/generateId';
-import { iBaseLogEventProps, iLogEvent } from './interfaces/log-interfaces';
+import {
+  iAddLogEvent, iAddLogEventProps, iBaseLogEventConstructorProps, iLogEvent
+} from './interfaces/log-interfaces';
+import addEventDescriber from './utils/event-describers/addEventDescriber';
 import { getDateWithNanoSecondTimeStamp } from './utils/getNanoSecondTime';
 
 // ? this doesnt seem right, why does this need to extend props made for itself?
-interface iProps extends iBaseLogEventProps {
+/*interface iProps extends iBaseLogEventProps {
 	operation: LogOperationUnion;
-}
+}*/
 
 // todo use strategy pattern for description function
 
-export default abstract class BaseLogEvent<T> implements iLogEvent {
+export default class BaseLogEvent<V> implements iLogEvent<V> {
 	public date: Date;
-	description?: string;
+	description: string;
 	public id: string;
+	newValue: V;
+	oldValue: V;
 	public operation: LogOperationUnion;
 	public property: string;
 	public timeStamp: bigint;
 
 	public abstract describe(): string;
 
-	constructor({ operation, description, property }: iProps) {
+	private constructor(props: iBaseLogEventConstructorProps<V>) {
+		const { operation, property, describer, newValue, oldValue } = props;
 		this.id = generateId();
+
 		this.operation = operation;
 		this.property = property;
+		this.newValue = newValue;
+		this.oldValue = oldValue;
 
-		this.description = this.describe(); // todo description function should be a property for the abstract, so it can be used here initially
+		this.description = describer(props);
 
 		// generate time stamp and save date object
 		const [date, nanoSecondTimeStamp] = getDateWithNanoSecondTimeStamp();
 		this.date = date;
 		this.timeStamp = nanoSecondTimeStamp;
+	}
+
+	static newAddEvent<V>(props: iAddLogEventProps<V>): iAddLogEvent<V> {
+		return new BaseLogEvent({ ...props, operation: 'ADD', describer: addEventDescriber, oldValue: nul });
 	}
 }
