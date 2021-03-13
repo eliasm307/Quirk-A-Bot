26b1f272177db0895288fa7c746aeecec8e7b945
Eli <13420359+eliasm307@users.noreply.github.com>
2021-03-13 00:27:06 +0000

    trying to use strategy pattern for logs, this produces issues such as having to make new and old value nullable, so more type checks would be required and there are other issues, its better to have separate classes  with specific responsibilities

---
+++ packages/data-model/src/classes/log/utils/event-describers/addEventDescriber.ts
@@ -0,0 +1,5 @@
+import { iBaseLogEventConstructorProps } from '../../interfaces/log-interfaces';
+
+export default function addEventDescriber<V>(props: iBaseLogEventConstructorProps<V>): string {
+	throw Error('Not implemented');
+}
---
+++ packages/data-model/src/classes/log/utils/event-describers/deleteEventDescriber.ts
@@ -0,0 +1,5 @@
+import { iBaseLogEventConstructorProps } from '../../interfaces/log-interfaces';
+
+export default function deleteEventDescriber<V>(props: iBaseLogEventConstructorProps<V>): string {
+	throw Error('Not implemented');
+}
