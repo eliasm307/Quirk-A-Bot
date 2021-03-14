af8abcb05d78bf53ae950713daf68878e323bc06
Eli <13420359+eliasm307@users.noreply.github.com>
2021-03-14 23:06:38 +0000

    added todo

--- packages/data-model/src/index.ts
+++ packages/data-model/src/index.ts
@@ -2,4 +2,6 @@ import CharacterSheet from './classes/characterSheet/CharacterSheet';
 
 console.warn(__filename, 'init');
 
+// todo define exports which include protection based on the signed in user, ie a user can only access games or character sheets they have access to
+
 export { CharacterSheet };
