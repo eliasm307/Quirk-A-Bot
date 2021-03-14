1295286a0716d364fad67543fb9a7b6064a20acf
Eli <13420359+eliasm307@users.noreply.github.com>
2021-03-14 19:27:26 +0000

    tweaked build script and tweaked gitignore for firebase files

--- packages/web-client/src/App.tsx
+++ packages/web-client/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>
-					Edit <code>src/App.js</code> and save to reload.
+					Editx <code>src/App.js</code> and save to reload.
 				</p>
 				<p>Charactersheet data: {JSON.stringify(data)}</p>
 				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
