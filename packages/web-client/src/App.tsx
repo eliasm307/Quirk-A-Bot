0cc1be22b7c44919fc7650bdd8cd0b8039d4ad7f
Eli <13420359+eliasm307@users.noreply.github.com>
2021-03-15 19:00:24 +0000

    web-client local dev and firebase deploy tested

--- packages/web-client/src/App.tsx
+++ packages/web-client/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>
-					Editx <code>src/App.js</code> and save to reload.
+					Edity <code>src/App.js</code> and save to reload.
 				</p>
 				<p>Charactersheet data: {JSON.stringify(data)}</p>
 				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
