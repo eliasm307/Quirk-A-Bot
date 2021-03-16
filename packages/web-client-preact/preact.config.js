ff6768d14e802b8194996fa4ced95bc17b118419
Eli <13420359+eliasm307@users.noreply.github.com>
2021-03-16 15:18:04 +0000

    centralised firebase emulator configs in firebase-utils

--- packages/web-client-preact/preact.config.js
+++ packages/web-client-preact/preact.config.js
@@ -1,6 +1,6 @@
 export default (config, env, helpers) => {
-	let { rule } = helpers.getLoadersByName(config, 'babel-loader')[0];
-	let babelConfig = rule.options;
+	// let { rule } = helpers.getLoadersByName(config, 'babel-loader')[0];
+	// let babelConfig = rule.options;
 
 	// console.warn(__filename, { babelConfig });
 
