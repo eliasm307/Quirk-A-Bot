d456391dce729ece52abdb058016d3ca6ce57e67
Eli <13420359+eliasm307@users.noreply.github.com>
2021-05-02 13:22:02 +0100

    moved files from data-model-firestore

--- packages/common/src/declarations/interfaces.ts
+++ packages/common/src/declarations/interfaces.ts
@@ -1,55 +1,77 @@
 export interface iHasGetData<D> {
-	readonly data: () => D;
+  readonly data: () => D;
 }
 
 export interface iCanHaveGetData<D> {
-	data?: () => D;
+  data?: () => D;
 }
 
 export interface iCanHaveSaveAction {
-	saveAction?: () => boolean;
+  saveAction?: () => boolean;
 }
 
 export interface iCanDescribe {
-	describe(): string;
+  describe(): string;
 }
 
 export interface iHasSaveAction {
-	saveAction: () => boolean;
+  saveAction: () => boolean;
 }
 
-export interface iBaseCollection<K extends string, SetValueType, ReturnValueType, CollectionType> {
-	readonly size: number;
+export interface iBaseCollection<
+  K extends string,
+  SetValueType,
+  ReturnValueType,
+  CollectionType
+> {
+  readonly size: number;
 
-	delete(key: K): CollectionType;
-	get(key: K): ReturnValueType | void;
-	has(key: K): boolean;
-	/**
-	 * Update trait value if it exists, otherwise add a new one
-	 * @param name name of trait to edit or create
-	 * @param newValue value to assign
-	 */
-	set(key: K, value: SetValueType): CollectionType;
-	toArray(): ReturnValueType[];
+  delete(key: K): CollectionType;
+  get(key: K): ReturnValueType | void;
+  has(key: K): boolean;
+  /**
+   * Update trait value if it exists, otherwise add a new one
+   * @param name name of trait to edit or create
+   * @param newValue value to assign
+   */
+  set(key: K, value: SetValueType): CollectionType;
+  toArray(): ReturnValueType[];
 }
 
 // ? should this be renamed to parentId?
 export interface iHasParentPath {
-	/** Path from the root to reach the parent of this item */
-	parentPath: string;
+  /** Path from the root to reach the parent of this item */
+  parentPath: string;
 }
 
 export interface iCanHaveParentPath {
-	/** Path from the root to reach the parent of this item */
-	parentPath?: string;
+  /** Path from the root to reach the parent of this item */
+  parentPath?: string;
 }
 
 // ? should this be renamed to id?
 export interface iHasPath {
-	/** Path from the root to reach this item */
-	path: string;
+  /** Path from the root to reach this item */
+  path: string;
 }
 
 export interface iHasCleanUp {
-	cleanUp(): boolean;
+  cleanUp(): boolean;
+}
+
+export interface iSubDocument<V> {
+  readonly parentDocumentPath: string;
+
+  data: V;
+
+  delete(): void;
+  setDataLocallyOnly(newValue: V): void;
+}
+
+export interface iDocumentGroup<K extends string, V> {
+  readonly data?: Record<K, V>;
+
+  cleanUp(): void;
+  get(key: K): iSubDocument<V> | undefined;
+  toArray(): iSubDocument<V>[];
 }
