0fe78aeffe96aceb73a51afbccff63420dd70b14
Eli <13420359+eliasm307@users.noreply.github.com>
2021-05-04 22:11:44 +0100

    resolved NumberTrait.InMemory.test async issues

--- packages/data-model/src/classes/traits/trait/NumberTrait.InMemory.test.ts
+++ packages/data-model/src/classes/traits/trait/NumberTrait.InMemory.test.ts
@@ -8,45 +8,92 @@ const parentPath = "NumberTrait-InMemory-test";
 // todo replace all jest "test" functions with "it" functions
 
 describe("Number trait with in memory data storage", () => {
-  const trait1 = new NumberTrait<string>({
-    max: 10,
-    name: "numberTrait1",
-    value: 5.2,
-    traitDataStorageInitialiser,
-    parentPath,
-    loggerCreator: null,
-  });
-  const trait2 = new NumberTrait<string>({
-    max: 10,
-    name: "numberTrait2",
-    value: 5.8,
-    traitDataStorageInitialiser,
-    parentPath,
-    loggerCreator: null,
-  });
   it("rounds values on instantiation", () => {
+    const trait1 = new NumberTrait<string>({
+      max: 10,
+      name: "numberTrait1",
+      value: 5.2,
+      traitDataStorageInitialiser,
+      parentPath,
+      loggerCreator: null,
+    });
+
+    const trait2 = new NumberTrait<string>({
+      max: 10,
+      name: "numberTrait2",
+      value: 5.8,
+      traitDataStorageInitialiser,
+      parentPath,
+      loggerCreator: null,
+    });
     expect(trait1.value).toEqual(5);
     expect(trait2.value).toEqual(6);
   });
 
   it("rounds up values with a decimal portion equal to or greater than 0.5 on value modification", async () => {
-    await trait1.setValue(0.5); // trait 1 modification 1
+    expect.assertions(1);
+    const trait1 = new NumberTrait<string>({
+      max: 10,
+      name: "numberTrait1",
+      value: 5.2,
+      traitDataStorageInitialiser,
+      parentPath,
+      loggerCreator: null,
+    });
+
+    await trait1.setValue(0.5);
+
     expect(trait1.value).toEqual(1);
   });
 
-  it("rounds down values with a decimal portion less than 0.5 on modification", () => {
-    trait1.value = 0.4; // trait 1 modification 2
+  it("rounds down values with a decimal portion less than 0.5 on modification", async () => {
+    expect.assertions(1);
+
+    const trait1 = new NumberTrait<string>({
+      max: 10,
+      name: "numberTrait1",
+      value: 5.2,
+      traitDataStorageInitialiser,
+      parentPath,
+      loggerCreator: null,
+    });
+
+    await trait1.setValue(0.4);
     expect(trait1.value).toEqual(0);
   });
 
-  it("does not accept any value modifications above or belows the defined limit", () => {
-    trait1.value = 5; // trait 1 modification 3
-    trait1.value = 11;
-    trait1.value = -1;
+  it("does not accept any value modifications above or belows the defined limit", async () => {
+    expect.assertions(3);
+
+    const trait1 = new NumberTrait<string>({
+      max: 10,
+      name: "numberTrait1",
+      value: 5.2,
+      traitDataStorageInitialiser,
+      parentPath,
+      loggerCreator: null,
+    });
+
+    await trait1.setValue(5);
+    await trait1.setValue(11);
+    await trait1.setValue(-1);
     expect(trait1.value).toEqual(5);
   });
 
-  it("logs modifications", () => {
+  it("logs modifications", async () => {
+    expect.assertions(1);
+
+    const trait1 = new NumberTrait<string>({
+      max: 10,
+      name: "numberTrait1",
+      value: 5.2,
+      traitDataStorageInitialiser,
+      parentPath,
+      loggerCreator: null,
+    });
+    await trait1.setValue(1);
+    await trait1.setValue(2);
+    await trait1.setValue(3);
     expect(trait1.log.events.length).toEqual(3);
   });
 });
