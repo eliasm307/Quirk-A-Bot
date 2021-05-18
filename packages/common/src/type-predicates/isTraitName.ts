00df6350aac716fb918d91fc9227ab879b3b735a
Eli <13420359+eliasm307@users.noreply.github.com>
2021-05-18 17:59:12 +0100

    updated trait name predicates to use unkown input

--- packages/common/src/type-predicates/isTraitName.ts
+++ packages/common/src/type-predicates/isTraitName.ts
@@ -5,31 +5,41 @@ import {
   AttributeName, CoreNumberTraitName, CoreStringTraitName, DisciplineName, SkillName,
 } from '../declarations/typePredicates';
 
-export function isAttributeName(name: string): name is AttributeName {
+export function isAttributeName(name: unknown): name is AttributeName {
+  if (typeof name !== "string") return false;
+
   const allowedKeys: string[] = [...ATTRIBUTE_NAMES];
   return allowedKeys.indexOf(name) !== -1;
 }
 
-export function isSkillName(name: string): name is SkillName {
+export function isSkillName(name: unknown): name is SkillName {
+  if (typeof name !== "string") return false;
+
   const allowedKeys: string[] = [...SKILL_NAMES];
   return allowedKeys.indexOf(name) !== -1;
 }
 
-export function isDisciplineName(name: string): name is DisciplineName {
+export function isDisciplineName(name: unknown): name is DisciplineName {
+  if (typeof name !== "string") return false;
+
   const allowedKeys: string[] = [...DISCIPLINE_NAMES];
   return allowedKeys.indexOf(name) !== -1;
 }
 
 export function isCoreNumberTraitName(
-  name: string
+  name: unknown
 ): name is CoreNumberTraitName {
+  if (typeof name !== "string") return false;
+
   const allowedKeys: string[] = [...CORE_NUMBER_TRAIT_NAMES];
   return allowedKeys.indexOf(name) !== -1;
 }
 
 export function isCoreStringTraitName(
-  name: string
+  name: unknown
 ): name is CoreStringTraitName {
+  if (typeof name !== "string") return false;
+
   const allowedKeys: string[] = [...CORE_STRING_TRAIT_NAMES];
   return allowedKeys.indexOf(name) !== -1;
 }
