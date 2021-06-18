5ded723cbacc9e21c02c5d586acc02464f8d98c4
Eli <13420359+eliasm307@users.noreply.github.com>
2021-06-18 21:49:44 +0100

    CharacterSheetFirestoreCompositeModelReader extends AbstractModelReader

--- packages/data-model/src/classes/data-models/firestore-composite/CharacterSheetModelReader.ts
+++ packages/data-model/src/classes/data-models/firestore-composite/CharacterSheetModelReader.ts
@@ -1,61 +1,14 @@
-import { Observable } from 'rxjs';
-import { filter, map, scan } from 'rxjs/operators';
-
-import { iHasParentPath, valuesAreEqual } from '@quirk-a-bot/common';
+import { iHasParentPath } from '@quirk-a-bot/common';
 
 import { iHasId } from '../../../declarations/interfaces';
-import getFirestoreDocumentChangeObservable from '../../../utils/getFirestoreDocumentChangeObservable';
 import { isCharacterSheetData } from '../../../utils/type-predicates';
 import { iCharacterSheetData } from '../../character-sheet/interfaces/character-sheet-interfaces';
-import { createPath } from '../../data-storage/utils/createPath';
-import { BaseModelReader } from '../interfaces/interfaces';
+import AbstractModelReader from '../general/AbstractModelReader';
 
 interface Props extends iHasId, iHasParentPath {}
 
-export default class CharacterSheetFirestoreCompositeModelReader
-  implements BaseModelReader<iCharacterSheetData>
-{
-  // #firestoreDocumentRef: FirestoreDocumentReference;
-  #unsubscribers: (() => void)[] = [];
-  /** Outgoing changes to observers of this instance */
-  changes: Observable<iCharacterSheetData | undefined>;
-  id: string;
-  path: string;
-
+export default class CharacterSheetFirestoreCompositeModelReader extends AbstractModelReader<iCharacterSheetData> {
   constructor(props: Props) {
-    const { id, parentPath } = props;
-
-    this.id = id;
-
-    // todo assert id and parentPath are valid
-
-    // todo this should come from firestore composite utils which are used by all firestore composite data models
-    this.path = createPath(parentPath, id);
-
-    const outgoingUpdate$ = getFirestoreDocumentChangeObservable({
-      documentPath: this.path,
-      dataPredicate: isCharacterSheetData,
-    });
-
-    this.changes = outgoingUpdate$.pipe(
-      scan(
-        ({ data: lastData }, data) => ({
-          data,
-          hasChanges: !valuesAreEqual(lastData, data),
-        }),
-        {} as { data: iCharacterSheetData | undefined; hasChanges: boolean }
-      ),
-      filter(({ hasChanges }) => {
-        // if (!hasChanges) console.warn(`Ignoring change`);
-        return hasChanges;
-      }),
-      map(({ data }) => data)
-    );
-  }
-
-  /** Releases any resources */
-  dispose() {
-    console.log(`Dispose for path ${this.path}`);
-    this.#unsubscribers.forEach((unsubscribe) => unsubscribe());
+    super({ ...props, dataPredicate: isCharacterSheetData });
   }
 }
