dc831ace6186825e02a30d2b54abc237ed0b0300
Eli <13420359+eliasm307@users.noreply.github.com>
2021-06-19 17:53:34 +0100

    added UserFirestoreCompositeModelWriter

--- packages/data-model/src/classes/data-models/firestore-composite/UserModelWriter.ts
+++ packages/data-model/src/classes/data-models/firestore-composite/UserModelWriter.ts
@@ -1,23 +1,27 @@
-import { auth, iHasParentPath } from '@quirk-a-bot/common';
+import { auth } from '@quirk-a-bot/common';
 
-import { iHasId } from '../../../declarations/interfaces';
-import { iGameData } from '../../game/interfaces/game-interfaces';
 import { iUserData } from '../../user/interfaces';
 import { BaseModelWriter } from '../interfaces/interfaces';
-import AbstractModelWriter from './AbstractModelWriter';
-
-interface Props extends iHasId {}
 
 export default class UserFirestoreCompositeModelWriter
   implements BaseModelWriter<iUserData>
 {
   id: string;
 
-  constructor({ id }: Props) {
-    this.id = id;
+  constructor() {
+    if (!auth.currentUser)
+      throw Error(
+        `Cannot update user because no user is signed in, current user is ${typeof auth.currentUser}`
+      );
+
+    this.id = auth.currentUser.uid;
   }
 
-  update(newData: Partial<Omit<iGameData, "id">>): void {
-    throw new Error("Method not implemented.");
+  async update(newData: Partial<Omit<iUserData, "id">>): Promise<void> {
+    if (!auth.currentUser)
+      throw Error(
+        `Cannot update user with id ${this.id} because user is not signed in`
+      );
+    void auth.updateCurrentUser({ ...auth.currentUser, ...newData });
   }
 }
